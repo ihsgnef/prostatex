b33f55e6199bfdad4aceb4cbdcd36636a48c7b2c
mrsata <liuhanrick@gmail.com>
2021-09-01 10:51:27 -0500

    Minor change for debugging embeddings

--- encode.py
+++ encode.py
@@ -6,13 +6,21 @@ import pytorch_lightning as pl
 from wang_args import WangClassifier
 import argparse
 
-args = argparse.Namespace(mri_sequences='TBAKDEF', data_sequences='TBAKDEF', valid_dir='tbakd3_npy/uint8/5folds/4/train_bal', eval_batch_size=1, pooling=True)
-ckpt = '/net/scratch/hanliu/radiology/prostatex/results/wang-fold-bal/z4ktwfw9/checkpoints/epoch=102-valid_acc=0.81.ckpt' # splendid-night-1029
+split = 'valid_bal' # train_bal
+# args = argparse.Namespace(mri_sequences='TBAKDEF', data_sequences='TBAKDEF', valid_dir='tbakd3_npy/uint8/5folds/4/train_bal', eval_batch_size=1, pooling=False, embed_dim=10)
+args = argparse.Namespace(mri_sequences='ABDEFKT', data_sequences='TBAKDEF', valid_dir='tbakd3_npy/5folds/4/{}'.format(split), eval_batch_size=1, pooling=False, embed_dim=10)
+# args = argparse.Namespace(mri_sequences='ABDEFKT', data_sequences='TBAKDEF', valid_dir='tbakd3_npy/5folds/4/{}'.format(split), eval_batch_size=1, pooling=True, embed_dim=10)
+# ckpt = '/net/scratch/hanliu/radiology/prostatex/results/wang-fold-bal/z4ktwfw9/checkpoints/epoch=102-valid_acc=0.81.ckpt' # splendid-night-1029
+ckpt = '/net/scratch/hanliu/radiology/prostatex/results/wang-emb10/3p0j02wg/checkpoints/epoch=195-valid_auc=0.92.ckpt' # bal_emb10
+# ckpt = '/net/scratch/hanliu/radiology/prostatex/results/wang-emb10/1t9z4936/checkpoints/epoch=81-valid_loss=2.15.ckpt' # emb10
 model = WangClassifier.load_from_checkpoint(ckpt, **vars(args))
 _ = model.eval()
+print(model.w_ensemble.numpy())
+# w_e = np.array([0.11010821, 0.1750904, 0, 0, 0, 0.5072202 , 0.20758119, 0]) # emb10_bal
+# w_e = np.array([0, 0.30813366, 0, 0, 0, 0.31353357, 0, 0.37833276]) # emb10
 
-batches = list(iter(model.valid_dataloader()))
-inputs = [b[0][:, model.mri_index] for b in batches]
+batches = list(iter(model.val_dataloader()))
+inputs = [b[0] for b in batches]
 labels = [b[1] for b in batches]
 fids = sorted(os.listdir(model.hparams.valid_dir+'/0')) + sorted(os.listdir(model.hparams.valid_dir+'/1'))
 fids = [fid.replace('.npy', '') for fid in fids]
@@ -26,6 +34,6 @@ labels = np.asarray([l.squeeze().detach().numpy() for l in labels])
 embeds = np.asarray([e.squeeze().detach().numpy() for e in embeds])
 preds = np.asarray([p.squeeze().detach().numpy() for p in preds])
 
-path = model.hparams.valid_dir.replace('train_bal', 'train_findings.pkl')
+path = model.hparams.valid_dir.replace(split, 'embs/{}_findings_emb10.pkl'.format(split))
 pickle.dump((fids, inputs, labels, embeds, preds), open(path, "wb"))
-print("Encoded training findings (fids, inputs, labels, embeds, preds) at " + path)
+print("Encoded {} findings (fids, inputs, labels, embeds, preds) at ".format(split) + path)
