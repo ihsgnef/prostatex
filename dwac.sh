747d3516c608a9032ab92121329c026daab14d21
mrsata <liuhanrick@gmail.com>
2021-09-01 10:50:28 -0500

    Add shell scripts to train wang and dwac

---
+++ dwac.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+#SBATCH --output=/home/hanliu/slurm_out/%j.%N.stdout
+#SBATCH --error=/home/hanliu/slurm_out/%j.%N.stderr
+#SBATCH --job-name=train
+#SBATCH --partition=general
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20000
+#SBATCH --chdir=/net/scratch/hanliu/radiology/prostatex
+# #SBATCH --array=0-4
+
+hostname
+echo $CUDA_VISIBLE_DEVICES
+
+# export PATH=$PATH:/usr/local/cuda/bin
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH=/usr/local/cuda/lib
+# export FOLD=$SLURM_ARRAY_TASK_ID
+export FOLD=4
+export msin=ABDEFKT
+export pzn=BEFK
+export tzn=ABFT
+
+# nvidia-smi -l 1 &
+
+/home/hanliu/anaconda3/bin/python dwac_args.py \
+  --mri_sequences=$msin \
+  --data_sequences=TBAKDEF \
+  --embed_dim=10 \
+  --wandb_mode=online \
+  --wandb_group=dwac-emb10-merge \
+  --output_dir=results/dwac-emb10-merge \
+  --train_dir=tbakd3_npy/5folds/$FOLD/train \
+  --valid_dir=tbakd3_npy/5folds/$FOLD/valid \
+  --dataloader_num_workers=8 \
+  --gpus=1 \
+  --seed=42 \
+  --max_epochs=200 \
+  --learning_rate=1e-4 \
+  --vertical_flip=0.5 \
+  --rotate=30 \
+  --scale=0.2 \
+  --train_batch_size=16 \
+  --do_train \
+  --pooling \
+  --merge_seq
---
+++ wang.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+#SBATCH --output=/home/hanliu/slurm_out/%j.%N.stdout
+#SBATCH --error=/home/hanliu/slurm_out/%j.%N.stderr
+#SBATCH --job-name=train
+#SBATCH --partition=general
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20000
+#SBATCH --chdir=/net/scratch/hanliu/radiology/prostatex
+# #SBATCH --array=0-4
+
+hostname
+echo $CUDA_VISIBLE_DEVICES
+
+# export PATH=$PATH:/usr/local/cuda/bin
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH=/usr/local/cuda/lib
+# export FOLD=$SLURM_ARRAY_TASK_ID
+export FOLD=4
+export msin=ABDEFKT
+export pzn=BEFK
+export tzn=ABFT
+
+# nvidia-smi -l 1 &
+
+/home/hanliu/anaconda3/bin/python wang_args.py \
+  --mri_sequences=$msin \
+  --data_sequences=TBAKDEF \
+  --fn_penalty=20 \
+  --embed_dim=10 \
+  --wandb_mode=online \
+  --wandb_group=wang-emb10 \
+  --output_dir=results/wang-emb10 \
+  --train_dir=tbakd3_npy/5folds/$FOLD/train \
+  --valid_dir=tbakd3_npy/5folds/$FOLD/valid \
+  --dataloader_num_workers=8 \
+  --gpus=1 \
+  --seed=42 \
+  --max_epochs=200 \
+  --learning_rate=1e-4 \
+  --vertical_flip=0.5 \
+  --rotate=30 \
+  --scale=0.2 \
+  --train_batch_size=16 \
+  --eval_batch_size=-1 \
+  --do_train \
+  --pooling \
+
+  # --wandb_group=wang-tzn-fold-bal \
+  # --output_dir=results/wang-tzn-fold-bal \
+  # --train_dir=tbakd3_npy/tz/5folds/$FOLD/train_bal \
+  # --valid_dir=tbakd3_npy/tz/5folds/$FOLD/valid_bal \
